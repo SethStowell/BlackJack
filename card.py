ff321db36a233afc9cd04845342a3dfa5498ec03
SethStowell <seth.stowell@llwarriors.org>
2022-04-12 08:08:08 -0500

    Initial commit

---
+++ card.py
@@ -0,0 +1,15 @@
+class Card(object):
+
+
+
+
+    def __init__(self):
+        self.card = card
+        self.suit = suit
+        self.unit = unit
+        self.hardValue = hardValue
+        self.softValue = softValue
+
+    def __str__(self):
+        return f''
+
---
+++ deck.py
@@ -0,0 +1,16 @@
+class Deck(object):
+
+    suits = [C, S, H, D]
+    units = [2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A]
+
+    def __init__(self):
+        self.card = card
+        self.suit = suit
+        self.unit = unit
+
+
+
+        for number in suits:
+            card = suit[1] +
+
+    def __str__(self):
