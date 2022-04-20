fd13e3cf7cf453ca83aec21d2faa0f43db90bf7b
SethStowell <seth.stowell@llwarriors.org>
2022-04-20 08:08:26 -0500

    updated hand.py

--- hand.py
+++ hand.py
@@ -27,5 +27,77 @@ class Hand(object):
     def get_value(self):
         return self.value
 
-    def bet(self):
-        pass
+    def hit(self, card):
+        """Adds a card to a hand."""
+        if self.can_hit():
+            self._cards.append(card)
+        else:
+            raise CallError('Cannot hit a closed hand')
+
+    def can_split(self):
+        """Checks to see if a hand is able to split."""
+        canSplit = False
+        if len(self._cards) == 2 and self._cards[0].hardValue == self._cards[1].hardValue:
+            canSplit = True
+        return canSplit
+
+    def split(self):
+        """Splits a hand into two separate ones."""
+        if self.can_split():
+            card = self._cards.pop()
+            self.isSplit = True
+        else:
+            raise CallError('Can only split 2 cards of the same value.')
+        return card
+
+    def stand(self):
+        """Disables a hand from being played."""
+        self.isDone = True
+
+    def can_double(self):
+        """Checks to see if a hand is able to double down."""
+        canDouble = False
+        if len(self._cards) == 2:
+            canDouble = True
+        return canDouble
+
+    def double_down(self, card, bet):
+        """Doubles down on a hand and hits one last time."""
+        if self.can_double():
+            self._bet += bet
+            self.hit(card)
+            self.isDoubled = True
+        else:
+            pass
+
+    def is_blackjack(self):
+        """Checks to see if a hand is blackjack."""
+        isBlackjack = False
+        if len(self._cards) == 2:
+            if self._cards[0].shortName and self._cards[1].shortName in ['A', 'K', 'Q', 'J', '10']:
+                if self.soft_value() == 21:
+                    isBlackjack = True
+        return isBlackjack
+
+    def is_busted(self):
+        """Checks to see if a hand is busted"""
+        isBusted = False
+        if self.hard_value() > 21:
+            isBusted = True
+        return isBusted
+
+    def is_doubled(self):
+        """Checks to see if a hand is doubled."""
+        return self.isDoubled
+
+    def is_split(self):
+        """Checks to see if a hand is split."""
+        return self.isSplit
+
+    def get_bet(self):
+        return self._bet
+
+    bet = property(get_bet)
+
+
+
